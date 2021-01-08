212307ec24c53be9905e1bee57525e618cab4c79
CraftyDragon678 <craftydragon678@gmail.com>
2021-01-08 09:12:00 +0000

    feat: send specific channel if already set

--- src/bot/util/send.ts
+++ src/bot/util/send.ts
@@ -1,22 +1,31 @@
 import { APIMessageContentResolvable, Collection, Guild, MessageAdditions, TextChannel } from 'discord.js';
 import { CommandoClient } from "discord.js-commando";
+import Channels from '../models/channelModel';
 
-const getDefaultChannel = (guild: Guild) => (
-  (guild.channels
-    .cache
-    .filter((channel) => (
-      (<typeof channel.type[]>['text', 'news', 'store']).includes(channel.type)
-        && !!guild.client.user
-        && !!channel.permissionsFor(guild.client.user)?.has('VIEW_CHANNEL')
-        && !!channel.permissionsFor(guild.client.user)?.has('SEND_MESSAGES')
-    )) as Collection<string, TextChannel>)
-    .sort((a, b) => a.rawPosition - b.rawPosition)
-    .first()
-);
+const getDefaultChannel = async (guild: Guild) => {
+  const row = await Channels.findById(guild.id);
+  if (row) {
+    const channel = guild.channels.cache.get(row.channel);
+    if (channel) return channel as TextChannel;
+  }
 
-const send = (client: CommandoClient, content: APIMessageContentResolvable | MessageAdditions) => {
-  client.guilds.cache.forEach((guild) => {
-    const channel = getDefaultChannel(guild);
+  return (
+    (guild.channels
+      .cache
+      .filter((channel) => (
+        (<typeof channel.type[]>['text', 'news', 'store']).includes(channel.type)
+          && !!guild.client.user
+          && !!channel.permissionsFor(guild.client.user)?.has('VIEW_CHANNEL')
+          && !!channel.permissionsFor(guild.client.user)?.has('SEND_MESSAGES')
+      )) as Collection<string, TextChannel>)
+      .sort((a, b) => a.rawPosition - b.rawPosition)
+      .first()
+  );
+};
+
+const send = async (client: CommandoClient, content: APIMessageContentResolvable | MessageAdditions) => {
+  client.guilds.cache.forEach(async (guild) => {
+    const channel = await getDefaultChannel(guild);
     if (!channel) return;
     channel.send(content);
   });
