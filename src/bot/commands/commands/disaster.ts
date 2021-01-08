96fb080dc465b042af47f73b4788df42062d5a98
CraftyDragon678 <craftydragon678@gmail.com>
2021-01-08 11:05:12 +0000

    feat(disaster): get location from db

--- src/bot/commands/commands/disaster.ts
+++ src/bot/commands/commands/disaster.ts
@@ -3,6 +3,13 @@ import { Command, CommandoClient, CommandoMessage } from 'discord.js-commando';
 import fetch from 'node-fetch';
 import { MessageEmbed } from 'discord.js';
 import disasterData from '@src/bot/data/commands/disaster';
+import Locations from '@src/bot/models/locationMode';
+
+const getLocation = async (userId: string, location: string) => {
+  if (location) return location;
+  const row = await Locations.findById(userId);
+  return row?.location.split(' ')[0] || '';
+};
 
 export default class DisasterCommand extends Command {
   constructor(client: CommandoClient) {
@@ -23,7 +30,8 @@ export default class DisasterCommand extends Command {
     });
   }
 
-  async run(msg: CommandoMessage, { location }: { location: string }) {
+  async run(msg: CommandoMessage, { location: _location }: { location: string }) {
+    const location = await getLocation(msg.author.id, _location);
     if (!location) {
       return msg.channel.send(stripIndents`
         명령어 사용법 : \`${msg.guild?.commandPrefix || this.client.commandPrefix}재난문자 [지역]\`
