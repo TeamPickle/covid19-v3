dcc598957019bc5d06b941617d2153e1a4d67aba
CraftyDragon678 <craftydragon678@gmail.com>
2021-01-09 01:07:41 +0000

    fix(prefix & resetprefix): check permission

--- src/bot/commands/util/resetprefix.ts
+++ src/bot/commands/util/resetprefix.ts
@@ -14,7 +14,7 @@ export default class ResetPrefixCommand extends Command {
   }
 
   async run(msg: CommandoMessage) {
-    if (msg.member.permissions.has('ADMINISTRATOR')) {
+    if (!msg.member.permissions.has('ADMINISTRATOR')) {
       return msg.channel.send('서버관리자만 접두사를 변경할 수 있습니다.');
     }
     const { guild } = msg;
