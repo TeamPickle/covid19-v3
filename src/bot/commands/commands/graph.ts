c2c51e652c6515624a2b6f126249ab34af622114
CraftyDragon678 <craftydragon678@gmail.com>
2021-01-09 07:19:32 +0000

    refactor: separate make graph function

--- src/bot/commands/commands/graph.ts
+++ src/bot/commands/commands/graph.ts
@@ -1,20 +1,6 @@
 import { Command, CommandoClient, CommandoMessage } from 'discord.js-commando';
 import { MessageAttachment } from 'discord.js';
-import { CanvasRenderService } from 'chartjs-node-canvas';
-import Chart from '@src/bot/models/chartModel';
-
-const getGraphData = async () => {
-  const data = await Chart
-    .find()
-    .limit(7)
-    .sort('-date');
-  return data.map((e) => ({
-    confirmed: e.confirmed,
-    released: e.released,
-    death: e.death,
-    date: e.date,
-  }));
-};
+import makeGraph from '@src/bot/util/graph';
 
 export default class GraphCommand extends Command {
   constructor(client: CommandoClient) {
@@ -28,92 +14,6 @@ export default class GraphCommand extends Command {
   }
 
   run = async (msg: CommandoMessage) => {
-    const canvasRenderService = new CanvasRenderService(640, 480, (chartJS) => {
-      chartJS.plugins.register({
-        beforeDraw: (chart) => {
-          const { ctx } = chart;
-          if (!ctx || !chart.width || !chart.height) return;
-          ctx.fillStyle = 'white';
-          ctx.fillRect(0, 0, chart.width, chart.height);
-        },
-        afterDraw: (chart) => {
-          const { ctx } = chart;
-          if (!ctx || !chart.data.datasets) return;
-
-          ctx.textAlign = 'center';
-          chart.data.datasets.forEach((dataset, datasetIndex) => {
-            const meta = chart.getDatasetMeta(datasetIndex);
-            meta.data.forEach((bar, index) => {
-              if (!dataset.data) return;
-              const data = dataset.data[index];
-              if (!data || typeof data !== 'number' || !dataset.backgroundColor) return;
-              ctx.fillStyle = dataset.backgroundColor.toString();
-              ctx.fillText(
-                data.toFixed(), bar._model.x + (datasetIndex - 1) * 20, bar._model.y - 10,
-              );
-            });
-          });
-        },
-      });
-    });
-
-    const data = await getGraphData();
-
-    const buffer = await canvasRenderService.renderToBuffer({
-      type: 'line',
-      data: {
-        labels: data.map((e) => e.date),
-        datasets: [{
-          label: '신규확진',
-          data: data.map((e) => e.confirmed),
-          fill: false,
-          backgroundColor: 'darkred',
-          borderColor: 'red',
-          lineTension: 0.3,
-        }, {
-          label: '격리해제',
-          data: data.map((e) => e.released),
-          fill: false,
-          backgroundColor: 'darkgreen',
-          borderColor: 'green',
-          lineTension: 0.3,
-        }, {
-          label: '사망',
-          data: data.map((e) => e.death),
-          fill: false,
-          backgroundColor: 'darkslategray',
-          borderColor: 'gray',
-          lineTension: 0.3,
-          yAxisID: 'sub',
-        }],
-      },
-      options: {
-        scales: {
-          xAxes: [{
-            type: 'time',
-            distribution: 'series',
-            time: {
-              unit: 'day',
-              displayFormats: {
-                day: 'YYYY-MM-DD',
-              },
-            },
-            offset: true,
-          }],
-          yAxes: [{
-            ticks: {
-              beginAtZero: true,
-            },
-          }, {
-            id: 'sub',
-            position: 'right',
-            ticks: {
-              beginAtZero: true,
-            },
-          }],
-        },
-      },
-    });
-    return msg.channel.send(new MessageAttachment(buffer));
+    return msg.channel.send(new MessageAttachment(await makeGraph()));
   }
 }
---
+++ src/bot/util/graph.ts
@@ -0,0 +1,107 @@
+import { CanvasRenderService } from 'chartjs-node-canvas';
+import Chart from '@src/bot/models/chartModel';
+import { ThenArg } from '@src/types/util';
+
+const getGraphData = async () => {
+  const data = await Chart
+    .find()
+    .limit(7)
+    .sort('-date');
+  return data.map((e) => ({
+    confirmed: e.confirmed,
+    released: e.released,
+    death: e.death,
+    date: e.date,
+  }));
+};
+
+const drawGraph = (data: ThenArg<ReturnType<typeof getGraphData>>) => {
+  const canvasRenderService = new CanvasRenderService(640, 480, (chartJS) => {
+    chartJS.plugins.register({
+      beforeDraw: (chart) => {
+        const { ctx } = chart;
+        if (!ctx || !chart.width || !chart.height) return;
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0, 0, chart.width, chart.height);
+      },
+      afterDraw: (chart) => {
+        const { ctx } = chart;
+        if (!ctx || !chart.data.datasets) return;
+
+        ctx.textAlign = 'center';
+        chart.data.datasets.forEach((dataset, datasetIndex) => {
+          const meta = chart.getDatasetMeta(datasetIndex);
+          meta.data.forEach((bar, index) => {
+            if (!dataset.data) return;
+            const data = dataset.data[index];
+            if (!data || typeof data !== 'number' || !dataset.backgroundColor) return;
+            ctx.fillStyle = dataset.backgroundColor.toString();
+            ctx.fillText(
+              data.toFixed(), bar._model.x + (datasetIndex - 1) * 20, bar._model.y - 10,
+            );
+          });
+        });
+      },
+    });
+  });
+
+  return canvasRenderService.renderToBuffer({
+    type: 'line',
+    data: {
+      labels: data.map((e) => e.date),
+      datasets: [{
+        label: '신규확진',
+        data: data.map((e) => e.confirmed),
+        fill: false,
+        backgroundColor: 'darkred',
+        borderColor: 'red',
+        lineTension: 0.3,
+      }, {
+        label: '격리해제',
+        data: data.map((e) => e.released),
+        fill: false,
+        backgroundColor: 'darkgreen',
+        borderColor: 'green',
+        lineTension: 0.3,
+      }, {
+        label: '사망',
+        data: data.map((e) => e.death),
+        fill: false,
+        backgroundColor: 'darkslategray',
+        borderColor: 'gray',
+        lineTension: 0.3,
+        yAxisID: 'sub',
+      }],
+    },
+    options: {
+      scales: {
+        xAxes: [{
+          type: 'time',
+          distribution: 'series',
+          time: {
+            unit: 'day',
+            displayFormats: {
+              day: 'YYYY-MM-DD',
+            },
+          },
+          offset: true,
+        }],
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+          },
+        }, {
+          id: 'sub',
+          position: 'right',
+          ticks: {
+            beginAtZero: true,
+          },
+        }],
+      },
+    },
+  });
+};
+
+const makeGraph = async () => drawGraph(await getGraphData());
+
+export default makeGraph;
