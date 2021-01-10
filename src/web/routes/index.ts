5957084d6277c521474f7a9173a0f4dbc87009a3
CraftyDragon678 <craftydragon678@gmail.com>
2021-01-10 10:16:09 +0900

    feat(web): add more information in /status

--- src/web/routes/index.ts
+++ src/web/routes/index.ts
@@ -1,12 +1,20 @@
 import client from '@src/bot';
+import Autocalls from '@src/bot/models/autocallModel';
 import { Router } from 'express';
+import expressAsyncHandler from 'express-async-handler';
 
 const router = Router();
 
-router.get('/status', (req, res) => {
-  res.json({
-    guildCounts: client.guilds.cache.size,
+router.get('/status', expressAsyncHandler(async (req, res) => {
+  return res.json({
+    guildSize: client.guilds.cache.size,
+    channelSize: client.channels.cache.size,
+    memberSize: client.guilds.cache.reduce((prev, curr) => prev + curr.memberCount, 0),
+    autocallSize: (await Autocalls.find({})).length,
+    userSize: client.users.cache.size,
+    uptime: client.uptime,
+    readyAt: client.readyAt,
   });
-});
+}));
 
 export default router;
