ce036e0a65499bef8858ad25e989ed74063176b1
CraftyDragon678 <craftydragon678@gmail.com>
2021-01-10 10:00:44 +0900

    feat: add status router

--- src/web/routes/index.ts
+++ src/web/routes/index.ts
@@ -1,5 +1,12 @@
+import client from '@src/bot';
 import { Router } from 'express';
 
 const router = Router();
+
+router.get('/status', (req, res) => {
+  res.json({
+    guildCounts: client.guilds.cache.size,
+  });
+});
 
 export default router;
