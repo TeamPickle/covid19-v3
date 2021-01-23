6ac214a2729d08904708abaf41f2295944651ae5
CraftyDragon678 <craftydragon678@gmail.com>
2021-01-23 23:25:13 +0900

    fix: graphic

--- src/bot/commands/commands/graphic.ts
+++ src/bot/commands/commands/graphic.ts
@@ -56,18 +56,21 @@ const makeImage = async (data: ThenArg<ReturnType<typeof parseNcov>>) => {
   context.textBaseline = 'hanging';
 
   locations.slice(1).forEach((v) => {
-    context.font = 'bold 48px NotoSans';
-    const measure = context.measureText(`${data[v].confirmed}`);
-    if (graphicData[v].position === "right") {
+    if (graphicData[v].position === 'right') {
+      context.font = 'bold 48px NotoSans';
+      context.textAlign = 'left';
+      const measure = context.measureText(`${data[v].confirmed}`);
       context.fillText(`${data[v].confirmed}`, graphicData[v].textX, graphicData[v].textY);
       context.font = '24px NotoSans';
       context.fillText(`(총 ${data[v].confirmedAcc})`, graphicData[v].textX + measure.width + 10, graphicData[v].textY);
+      return;
     }
-    else {
-      context.fillText(`${data[v].confirmed}`, graphicData[v].textX - measure.width, graphicData[v].textY);
-      context.font = '24px NotoSans';
-      context.fillText(`(총 ${data[v].confirmedAcc})`, graphicData[v].textX - measure.width - 10, graphicData[v].textY);
-    }
+    context.font = 'bold 48px NotoSans';
+    context.textAlign = 'right';
+    const measure = context.measureText(`${data[v].confirmed}`);
+    context.fillText(`${data[v].confirmed}`, graphicData[v].textX + 220, graphicData[v].textY);
+    context.font = '24px NotoSans';
+    context.fillText(`(총 ${data[v].confirmedAcc})`, graphicData[v].textX + 210 - measure.width, graphicData[v].textY);
   });
 
   return canvas.toBuffer();
