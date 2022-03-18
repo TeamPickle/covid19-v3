4919feac5f897afecb9234a9b4dedf2522607440
cra1nbow <dev@cra1nbow.is>
2022-03-18 10:59:47 +0900

    chore(deps): upgrade chart.js

--- src/bot/util/graph.ts
+++ src/bot/util/graph.ts
@@ -1,4 +1,4 @@
-import { CanvasRenderService } from 'chartjs-node-canvas';
+import { ChartJSNodeCanvas } from 'chartjs-node-canvas';
 
 const drawGraph = (
   data: {
@@ -8,40 +8,48 @@ const drawGraph = (
     date: Date;
   }[],
 ) => {
-  const canvasRenderService = new CanvasRenderService(640, 480, (chartJS) => {
-    chartJS.plugins.register({
-      beforeDraw: (chart) => {
-        const { ctx } = chart;
-        if (!ctx || !chart.width || !chart.height) return;
-        ctx.fillStyle = 'white';
-        ctx.fillRect(0, 0, chart.width, chart.height);
-      },
-      afterDraw: (chart) => {
-        const { ctx } = chart;
-        if (!ctx || !chart.data.datasets) return;
+  const chartJSNodeCanvas = new ChartJSNodeCanvas({
+    width: 640,
+    height: 480,
+    chartCallback: (chartJS) => {
+      chartJS.plugins.register({
+        beforeDraw: (chart) => {
+          const { ctx } = chart;
+          if (!ctx || !chart.width || !chart.height) return;
+          ctx.fillStyle = 'white';
+          ctx.fillRect(0, 0, chart.width, chart.height);
+        },
+        afterDraw: (chart) => {
+          const { ctx } = chart;
+          if (!ctx || !chart.data.datasets) return;
 
-        ctx.textAlign = 'center';
-        chart.data.datasets.forEach((dataset, datasetIndex) => {
-          const meta = chart.getDatasetMeta(datasetIndex);
-          meta.data.forEach((bar, index) => {
-            if (!dataset.data) return;
-            const data = dataset.data[index];
-            if (!data || typeof data !== 'number' || !dataset.backgroundColor) {
-              return;
-            }
-            ctx.fillStyle = dataset.backgroundColor.toString();
-            ctx.fillText(
-              data.toFixed(),
-              bar._model.x + (datasetIndex - 1) * 20,
-              bar._model.y - 10,
-            );
+          ctx.textAlign = 'center';
+          chart.data.datasets.forEach((dataset, datasetIndex) => {
+            const meta = chart.getDatasetMeta(datasetIndex);
+            meta.data.forEach((bar, index) => {
+              if (!dataset.data) return;
+              const data = dataset.data[index];
+              if (
+                !data ||
+                typeof data !== 'number' ||
+                !dataset.backgroundColor
+              ) {
+                return;
+              }
+              ctx.fillStyle = dataset.backgroundColor.toString();
+              ctx.fillText(
+                data.toFixed(),
+                bar._model.x + (datasetIndex - 1) * 20,
+                bar._model.y - 10,
+              );
+            });
           });
-        });
-      },
-    });
+        },
+      });
+    },
   });
 
-  return canvasRenderService.renderToBuffer({
+  return chartJSNodeCanvas.renderToBuffer({
     type: 'line',
     data: {
       labels: data.map((e) => e.date),
